7504aae0f12b6838acc2ce0e9bf6e77456363aa6
itsJeremehhh <107006437+itsJeremehhh@users.noreply.github.com>
2022-07-05 16:58:21 -0700

    Add files via upload

---
+++ Map and Set Exercise.js
@@ -0,0 +1,56 @@
+//Maps and Sets Exercise
+
+//what does the following code return??
+new Set([1, 2, 3, 4])
+
+//should return [1, 2, 3, 4]
+//solution\\
+{1,2,3,4}
+
+//what does the following code return?
+[...new Set("referee")].join("")
+"referee"
+//solution\\
+"ref"
+
+// What does the Map m look like after running the following code?
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+
+m = false
+
+//solution\\
+0: {Array(3) => true}
+1: {Array(3) => false}
+
+//Write a function called hasDuplicate which accepts an array and returns true or false if that array contains a duplicate
+
+//i have no idea...
+
+//solution\\
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+// Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string
+
+//dont even know where to begin\\
+
+//solution\\
+function isVowel(char){
+    return "aeiou".includes(char);
+  }
+  
+  function vowelCount(str){
+    const vowelMap = new Map();
+    for(let char of str){
+      let lowerCaseChar = char.toLowerCase()
+      if(isVowel(lowerCaseChar)){
+        if(vowelMap.has(lowerCaseChar)){
+          vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+        } else {
+          vowelMap.set(lowerCaseChar, 1);
+        }
+      }
+    }
+    return vowelMap;
+  }
